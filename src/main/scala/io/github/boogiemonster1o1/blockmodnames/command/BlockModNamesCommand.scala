92a47609cc99ca9e0a8e0a20334f020997bab1e7
SD <harrypotter.starwars.shrish@gmail.com>
2020-12-07 22:11:17 +0530

    Rename

--- src/main/scala/io/github/boogiemonster1o1/blockmodnames/command/BlockModNamesCommand.scala
+++ src/main/scala/io/github/boogiemonster1o1/blockmodnames/command/BlockModNamesCommand.scala
@@ -4,19 +4,23 @@ import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
 import io.github.boogiemonster1o1.blockmodnames.BlockModNames
 import net.minecraft.server.command.{CommandManager, ServerCommandSource}
+import net.minecraft.text.LiteralText
+import net.minecraft.util.Formatting
 
 object BlockModNamesCommand {
 	def register(dispatcher: CommandDispatcher[ServerCommandSource]): Unit = {
-		dispatcher.register(CommandManager.literal("blockmodname")
+		dispatcher.register(CommandManager.literal("blockmodnames")
 			.`then`(CommandManager.literal("save")
-				.executes((_: CommandContext[ServerCommandSource]) => {
+				.executes((ctx: CommandContext[ServerCommandSource]) => {
 					BlockModNames.save()
+					ctx.getSource.sendFeedback(new LiteralText("Saved BlockModNames config").formatted(Formatting.YELLOW), true)
 					1
 				})
 			)
-			.`then`(CommandManager.literal("load")
-				.executes((_: CommandContext[ServerCommandSource]) => {
+			.`then`(CommandManager.literal("reload")
+				.executes((ctx: CommandContext[ServerCommandSource]) => {
 					BlockModNames.load()
+					ctx.getSource.sendFeedback(new LiteralText("Reloaded BlockModNames config").formatted(Formatting.YELLOW), true)
 					1
 				})
 			)
